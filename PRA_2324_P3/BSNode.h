0e23357cc31962422636dce3e439f0037544c457
Marcos Belda Martínez <mbelmar@etsinf.upv.es>
2024-01-02 18:26:40 +0100

    Implementación de la calse BSNode

---
+++ PRA_2324_P3/BSNode.h
@@ -0,0 +1,100 @@
+/*-----------------------------------------------------+
+ |     B S N O D E . H                                 |
+ +-----------------------------------------------------+
+ |     Versión     : P03_v1                            | 
+ |     Autor       : Marcos Belda Martínez             |
+ |     Asignatura  : PRA-GIIROB                        |     
+ |     Descripción : Interfaz BSNode                   |
+ +-----------------------------------------------------*/
+
+#ifndef BSNODE_H
+#define BSNODE_H
+
+#include <ostream>
+
+using namespace std;
+
+template <typename V> 
+class BSNode {
+	
+	public:
+	
+		/****************************************************************/
+		/*                       elem (atributo)                        */
+		/*--------------------------------------------------------------*/
+		/*                                                              */
+		/* DESCRIPCION:                                                 */
+		/* El elemento de tipo T almacenado en el nodo.                 */                                                 */
+		/*                                                              */
+		/****************************************************************/
+
+		T elem;
+	
+		/****************************************************************/
+		/*                       left (atributo)                        */
+		/*--------------------------------------------------------------*/
+		/*                                                              */
+		/* DESCRIPCION:                                                 */
+		/* Puntero al nodo sucesor izquierdo.                           */
+		/*                                                              */
+		/****************************************************************/		
+
+		BSNode<T>* left; 
+	
+		/****************************************************************/
+		/*                      rigth (atributo)                       */
+		/*--------------------------------------------------------------*/
+		/*                                                              */
+		/* DESCRIPCION:                                                 */
+		/* Puntero al nodo sucesor derecho.                             */
+		/*                                                              */
+		/****************************************************************/
+
+		BSNode<T>* right;
+	
+		/****************************************************************/
+		/*                     BSNode (constructor)                     */
+		/*--------------------------------------------------------------*/
+		/*                                                              */
+		/* DESCRIPCION:                                                 */
+		/* Método constructor que crea un BSNode con el elemento elem y */
+		/* los punteros a los nodos sucesores left y right              */
+		/* proporcionados (nulos por defecto).                          */
+		/*                                                              */
+		/* ENTRADA: T elem, BSNode<T>* left, BSNode<T>* right.          */
+		/*                                                              */
+		/* SALIDA: void.                                                */
+		/****************************************************************/
+
+		BSNode (T elem, BSNode<T>* left=nullptr, BSNode<T>* right=nullptr) {
+
+			this->elem = elem;
+			this->left = left;
+			this->rigth = rigth;
+		}
+	
+		/****************************************************************/
+		/*                           operator <<                        */
+		/*--------------------------------------------------------------*/
+		/*                                                              */
+		/* DESCRIPCION:                                                 */
+		/* Sobrecarga global del operador << para imprimir el nodo por  */
+		/* pantalla. Por simplicidad, puedes limitarte a imprimir el    */
+		/* atributo elem.                                               */
+		/*                                                              */
+		/* ENTRADA: T elem, BSNode<T>* left, BSNode<T>* right.          */
+		/*                                                              */
+		/* SALIDA: out.                                                 */
+		/****************************************************************/
+
+		friend ostream& operator<<(ostream &out, const BSNode<T> &bsn) {
+
+			out << "Elemento  -> " << bsn.elem << enld;
+			out << "Izquierda -> " << bsn.left << enld;
+			out << "Drerecha  -> " << bsn.rigth << enld;
+
+			return out;
+		}
+};
+
+#endif
